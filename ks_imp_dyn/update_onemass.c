c6bc2b5cbb0c25de7499ef0effe76907a1f705b1
ctpeterson <curtistaylorpetersonwork@gmail.com>
2024-09-19 11:16:59 -0400

    Restored update_onemass.c to version from 09/19/2024

--- ks_imp_dyn/update_onemass.c
+++ ks_imp_dyn/update_onemass.c
@@ -46,37 +46,36 @@ int update(){
     Real old_cg_time,next_cg_time = -1.0e6;
     imp_ferm_links_t** fn;
 
-    // HISQ smear
-    void smear(){
-        restore_fermion_links_from_site(fn_links, MILC_PRECISION);
-	    fn = get_fm_links(fn_links);
-    }
-
-    // CG solve
-    void solve(){
-        iters += ks_congrad( 
-            F_OFFSET(phi), F_OFFSET(xxx), mass, 
-		    niter, nrestart, rsqmin, MILC_PRECISION, 
-		    EVEN, &final_rsq, fn[0] 
-        );
-    }
-
     // Momentum update
     void update_t(int step, Real offset){
         next_cg_time = ((Real)step-offset)*epsilon;
 	    predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
-        smear(); solve();
-	    dslash_site(F_OFFSET(xxx), F_OFFSET(xxx), ODD, fn[0]);
+        restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+	    fn = get_fm_links(fn_links);
+     	iters += ks_congrad( 
+            F_OFFSET(phi), F_OFFSET(xxx), mass, 
+			niter, nrestart, rsqmin, MILC_PRECISION, 
+			EVEN, &final_rsq, fn[0] 
+        );
+	    dslash_site( F_OFFSET(xxx), F_OFFSET(xxx), ODD, fn[0]);
 	    cg_time = ((Real)step - offset)*epsilon;
         update_h(0.5*epsilon);
     }
 
+    // First smearing
+    restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+	fn = get_fm_links(fn_links);
+
     // Heatbath (momentum & fermion)
-    ranmom(); smear();
+    ranmom();
     grsource_imp(F_OFFSET(phi), mass, EVEN, fn[0]);
 
     // Calculate initial action & store field
-    solve();
+    iters += ks_congrad( 
+        F_OFFSET(phi), F_OFFSET(xxx), mass, 
+		niter, nrestart, rsqmin, MILC_PRECISION, 
+		EVEN, &final_rsq, fn[0] 
+    );
     startaction = d_action();
     gauge_field_copy(F_OFFSET(link[0]), F_OFFSET(old_link[0]));
 
@@ -84,9 +83,9 @@ int update(){
     for(step=1; step <= steps; step++){
         if (step == 1){update_u(lmbda*epsilon);}
         else {update_u(2.0*lmbda*epsilon);}
-        update_t(step,0.333);
+        update_t(step,0.25);
         update_u((1.0-2.0*lmbda)*epsilon);
-        update_t(step,0.666);
+        update_t(step,0.5);
         if (step == steps){update_u(lmbda*epsilon);}
     }
     reunitarize_ks();
@@ -94,7 +93,13 @@ int update(){
     // Calculate final action
     next_cg_time = steps*epsilon;
     predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
-    smear(); solve();
+    restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+    fn = get_fm_links(fn_links);
+    iters += ks_congrad( 
+        F_OFFSET(phi), F_OFFSET(xxx), mass,
+		niter, nrestart, rsqmin, MILC_PRECISION, 
+		EVEN, &final_rsq, fn[0] 
+    );
     cg_time = steps*epsilon;
     endaction = d_action();
 
@@ -104,7 +109,7 @@ int update(){
     dH = (double)(endaction-startaction);
     if(exp(-dH)<xrandom){
 	    if(steps > 0){
-	        gauge_field_copy(F_OFFSET(old_link[0]), F_OFFSET(link[0]));   
+	        gauge_field_copy( F_OFFSET(old_link[0]), F_OFFSET(link[0]) );   
             #ifdef FN
 	            invalidate_fermion_links(fn_links);
             #endif
@@ -113,7 +118,7 @@ int update(){
     }
     else {node0_printf("ACCEPT: delta S = %e\n",dH);}
 
-    // Finish up
+    // finish up
     if(steps > 0){return (iters/steps);}
     else {return(-99);}
 }
