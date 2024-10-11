9138a1c19115b0ca7f3fadb06c2a74a97bad8cc7
ctpeterson <curtistaylorpetersonwork@gmail.com>
2024-10-11 13:40:02 -0400

    Optimized 2nd-order Omelyan integrator & resolved difference w/ RHMC code w.r.t. timing

--- ks_imp_dyn/update_onemass.c
+++ ks_imp_dyn/update_onemass.c
@@ -6,7 +6,11 @@ structure as I could, even though I don't like how it was
 originally written. Does not include R-algorithm because nobody
 uses it anymore.
 
+Note: "steps" in the MILC convetion means "number of force evaluations",
+not the number of full Omelyan steps
+
 (Rewrite) author: Curtis Taylor Peterson
+Original author(s): Unknown :/
 
 ********************************************************************/
 
@@ -17,11 +21,15 @@ int update(){
     double d_action();
     int step,iters = 0;
     double startaction,endaction,xrandom;
-    Real lmbda = 0.1931833275037836;
-    Real final_rsq,cg_time,old_cg_time,next_cg_time;
+    Real lmbda = 0.8;//0.1931833275037836;
+    Real final_rsq;
+    Real cg_time = 0.0;
+    Real old_cg_time = -1.0e6;
+    Real next_cg_time = -1.0e6;
     imp_ferm_links_t** fn;
 
-    void update_t(int step, double offset){
+    // Momnetum update wrapper
+    void update_v(int step, double offset){
         next_cg_time = ((Real)step-offset)*epsilon;
 	    predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
         restore_fermion_links_from_site(fn_links, MILC_PRECISION);
@@ -33,39 +41,39 @@ int update(){
         );
 	    dslash_site( F_OFFSET(xxx), F_OFFSET(xxx), ODD, fn[0]);
 	    cg_time = ((Real)step-offset)*epsilon;
-        update_h(0.5*epsilon);
+        update_h(epsilon);
     }
 
+    // First link smearing
+    restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+	fn = get_fm_links(fn_links);
+
+    // Momentum/fermion heatbath
     ranmom();
-    for(step=1; step <= steps; step++){
-        if (step == 1){
-            restore_fermion_links_from_site(fn_links, MILC_PRECISION);
-	        fn = get_fm_links(fn_links);
-	        grsource_imp(F_OFFSET(phi), mass, EVEN, fn[0]); 
-	        old_cg_time = cg_time = -1.0e6;
-        }
+    grsource_imp(F_OFFSET(phi), mass, EVEN, fn[0]); 
+    
+    // Calculate initial Hamiltonian
+	iters += ks_congrad( 
+        F_OFFSET(phi), F_OFFSET(xxx), mass, 
+		niter, nrestart, rsqmin, MILC_PRECISION, 
+		EVEN, &final_rsq, fn[0] 
+    );
+    startaction = d_action();
 
-        if(step==1){
-	        restore_fermion_links_from_site(fn_links, MILC_PRECISION);
-	        fn = get_fm_links(fn_links);
-	        iters += ks_congrad( 
-                F_OFFSET(phi), F_OFFSET(xxx), mass, 
-				niter, nrestart, rsqmin, MILC_PRECISION, 
-				EVEN, &final_rsq, fn[0] 
-            );
-	        cg_time = 0.0;
-     	    startaction=d_action();
-	        gauge_field_copy(F_OFFSET(link[0]), F_OFFSET(old_link[0]));
-        }
+    // Save backup gauge field
+	gauge_field_copy(F_OFFSET(link[0]), F_OFFSET(old_link[0]));
 
-        if (step == 1){update_u(lmbda*epsilon);}
-        else {update_u(2.0*lmbda*epsilon);}
-        update_t(step,0.333);
-        update_u((1.0-2.0*lmbda)*epsilon);
-        update_t(step,0.667);
-        if (step == steps){update_u(lmbda*epsilon);}
+    // MILC's convention for a 2nd-order Omelyan integrator
+    for(step=2; step <= steps; step+=2){
+        if (step == 2){update_u(0.5*lmbda*epsilon);}
+        else {update_u(lmbda*epsilon);}
+        update_v(step,0.333);
+        update_u((2.0-lmbda)*epsilon);
+        update_v(step,0.667);
+        if (step == steps){update_u(0.5*lmbda*epsilon);}
     }
 
+    // Smear one last time & calculate final Hamiltonian
     next_cg_time = steps*epsilon;
     predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
     restore_fermion_links_from_site(fn_links, MILC_PRECISION);
@@ -78,6 +86,7 @@ int update(){
     cg_time = steps*epsilon;
     endaction = d_action();
 
+    // Metropolis
     if(this_node==0)xrandom = myrand(&node_prn);
     broadcast_float(&xrandom);
     if( exp( (double)(startaction-endaction) ) < xrandom ){
@@ -88,9 +97,7 @@ int update(){
         #endif
 	    node0_printf("REJECT: delta S = %e\n", (double)(endaction-startaction));
     }
-    else {
-	    node0_printf("ACCEPT: delta S = %e\n", (double)(endaction-startaction));
-    }
+    else {node0_printf("ACCEPT: delta S = %e\n", (double)(endaction-startaction));}
 
     if(steps > 0)return (iters/steps);
     else return(-99);
