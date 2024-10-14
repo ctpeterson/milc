5b729ca72fefcb4cb7d5537beb0f798ccb74dc51
ctpeterson <curtistaylorpetersonwork@gmail.com>
2024-10-14 16:45:15 -0400

    Added "3G1F" integrator back to update_onemass.c

--- ks_imp_dyn/update_h.c
+++ ks_imp_dyn/update_h.c
@@ -11,90 +11,85 @@
 #include "ks_imp_includes.h"	/* definitions files and prototypes */
 
 #ifdef ONEMASS
-void eo_fermion_force_oneterm( Real eps, Real weight, su3_vector *temp_x,
-			  int prec, fermion_links_t *fl )
-{
-  su3_vector *xxx[1] = { temp_x };
-  int nterms = 1;
-  Real residues[1] = { weight };
+  void eo_fermion_force_oneterm( Real eps, Real weight, su3_vector *temp_x,
+          int prec, fermion_links_t *fl )
+  {
+    su3_vector *xxx[1] = { temp_x };
+    int nterms = 1;
+    Real residues[1] = { weight };
 
-  eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
-  
-}
+    eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
+    
+  }
 
-void eo_fermion_force_oneterm_site( Real eps, Real weight, field_offset x_off,
-				    int prec, fermion_links_t *fl)
-{
-  su3_vector *temp_x = create_v_field_from_site_member(x_off);
-  su3_vector *xxx[1] = { temp_x };
-  int nterms = 1;
-  Real residues[1] = { weight };
+  void eo_fermion_force_oneterm_site( Real eps, Real weight, field_offset x_off,
+              int prec, fermion_links_t *fl)
+  {
+    su3_vector *temp_x = create_v_field_from_site_member(x_off);
+    su3_vector *xxx[1] = { temp_x };
+    int nterms = 1;
+    Real residues[1] = { weight };
 
-  eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
+    eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
 
-  destroy_v_field(temp_x);
-}
+    destroy_v_field(temp_x);
+  }
 #else
-void eo_fermion_force_twoterms( Real eps, Real weight1, Real weight2, 
-				su3_vector *x1_off, su3_vector *x2_off,
-				int prec, fermion_links_t *fl ){
-  su3_vector *xxx[2] = { x1_off, x2_off };
-  int nterms = 2;
-  Real residues[2] = { weight1, weight2 };
+  void eo_fermion_force_twoterms( Real eps, Real weight1, Real weight2, 
+          su3_vector *x1_off, su3_vector *x2_off,
+          int prec, fermion_links_t *fl ){
+    su3_vector *xxx[2] = { x1_off, x2_off };
+    int nterms = 2;
+    Real residues[2] = { weight1, weight2 };
 
-  eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
+    eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
+  }
+
+  void eo_fermion_force_twoterms_site( Real eps, Real weight1, Real weight2,
+              field_offset x1_off, field_offset x2_off,
+              int prec, fermion_links_t *fl)
+  {
+    su3_vector *x1 = create_v_field_from_site_member(x1_off);
+    su3_vector *x2 = create_v_field_from_site_member(x2_off);
+    su3_vector *xxx[2] = { x1, x2 };
+    int nterms = 2;
+    Real residues[2] = { weight1, weight2 };
+
+    eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
+
+    destroy_v_field(x1);
+    destroy_v_field(x2);
+  }
+#endif
+
+void update_h( Real eps ){update_h_gauge(eps); update_h_fermion(eps);}
+
+void update_h_gauge(Real eps){
+  rephase(OFF);
+  imp_gauge_force(eps,F_OFFSET(mom));
+  rephase(ON);
 }
 
-void eo_fermion_force_twoterms_site( Real eps, Real weight1, Real weight2,
-				     field_offset x1_off, field_offset x2_off,
-				     int prec, fermion_links_t *fl)
-{
-  su3_vector *x1 = create_v_field_from_site_member(x1_off);
-  su3_vector *x2 = create_v_field_from_site_member(x2_off);
-  su3_vector *xxx[2] = { x1, x2 };
-  int nterms = 2;
-  Real residues[2] = { weight1, weight2 };
-
-  eo_fermion_force_multi( eps, residues, xxx, nterms, prec, fl );
-
-  destroy_v_field(x1);
-  destroy_v_field(x2);
+void update_h_fermion(Real eps){
+  #ifdef FN
+    invalidate_fermion_links(fn_links);
+  #endif
+  restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+  #ifdef ONEMASS
+    eo_fermion_force_oneterm_site( 
+      eps, ((Real)nflavors)/4., 
+      F_OFFSET(xxx),
+			MILC_PRECISION, 
+      fn_links 
+    );
+  #else
+    eo_fermion_force_twoterms_site( 
+      eps, ((Real)nflavors1)/4., 
+			((Real)nflavors2)/4., 
+      F_OFFSET(xxx1), 
+			F_OFFSET(xxx2), 
+      MILC_PRECISION, 
+      fn_links 
+    );
+  #endif
 }
-#endif
-
-void update_h( Real eps ){
-  int ff_prec = MILC_PRECISION;  /* Just use prevailing precision for now */
-#ifdef FN
-//    free_fn_links(&fn_links);
-//    free_fn_links(&fn_links_dmdu0);
-  invalidate_fermion_links(fn_links);
-#endif
-    /* gauge field force */
-    rephase(OFF);
-    imp_gauge_force(eps,F_OFFSET(mom));
-    rephase(ON);
-    /* fermionic force */
-    /* First compute M*xxx in temporary vector xxx_odd */
-    /* See long comment at end of file */
-	/* The diagonal term in M doesn't matter */
-    restore_fermion_links_from_site(fn_links, MILC_PRECISION);
-
-#ifdef ONEMASS
-    eo_fermion_force_oneterm_site( eps, ((Real)nflavors)/4., F_OFFSET(xxx),
-				   ff_prec, fn_links );
-#else
-/**
-   eo_fermion_force_oneterm( eps, ((Real)nflavors1)/4., F_OFFSET(xxx1),
-        ff_prec, fn_links );
-    eo_fermion_force_oneterm( eps, ((Real)nflavors2)/4., F_OFFSET(xxx2),
-        ff_prec, fn_links );
-**/
-/**/
-    eo_fermion_force_twoterms_site( eps, ((Real)nflavors1)/4., 
-				    ((Real)nflavors2)/4., F_OFFSET(xxx1), 
-				    F_OFFSET(xxx2), ff_prec, fn_links );
-    /**/
-#endif
-} /* update_h */
-
-
--- ks_imp_dyn/update_onemass.c
+++ ks_imp_dyn/update_onemass.c
@@ -18,39 +18,68 @@ Original author(s): Unknown :/
 
 int update(){
     void predict_next_xxx(Real *oldtime,Real *newtime,Real *nexttime);
-    double d_action();
     int step,iters = 0;
+    double d_action();
     double startaction,endaction,xrandom;
-    Real lmbda = 0.8;//0.1931833275037836;
+    double nG = 1.0;
+    Real st1,st2;
+    Real lmbda = 0.1931833275037836;
+    Real beta = 0.1;
+    Real alpha = 0.1;
     Real final_rsq;
     Real cg_time = 0.0;
     Real old_cg_time = -1.0e6;
     Real next_cg_time = -1.0e6;
     imp_ferm_links_t** fn;
 
-    // Momnetum update wrapper
-    void update_v(int step, double offset){
-        next_cg_time = ((Real)step-offset)*epsilon;
-	    predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
-        restore_fermion_links_from_site(fn_links, MILC_PRECISION);
-	    fn = get_fm_links(fn_links);
-     	iters += ks_congrad( 
-            F_OFFSET(phi), F_OFFSET(xxx), mass, 
-			niter, nrestart, rsqmin, MILC_PRECISION, 
-			EVEN, &final_rsq, fn[0] 
-        );
-	    dslash_site( F_OFFSET(xxx), F_OFFSET(xxx), ODD, fn[0]);
-	    cg_time = ((Real)step-offset)*epsilon;
-        update_h(epsilon);
+    // Momentum update wrapper
+    void update_v(int step, double dtau, double offset){
+      next_cg_time = ((Real)step-offset)*epsilon;
+	  predict_next_xxx(&old_cg_time,&cg_time,&next_cg_time);
+      restore_fermion_links_from_site(fn_links, MILC_PRECISION);
+	  fn = get_fm_links(fn_links);
+      iters += ks_congrad( 
+        F_OFFSET(phi), F_OFFSET(xxx), mass, 
+		niter, nrestart, rsqmin, MILC_PRECISION, 
+		EVEN, &final_rsq, fn[0] 
+      );
+	  dslash_site( F_OFFSET(xxx), F_OFFSET(xxx), ODD, fn[0]);
+	  cg_time = ((Real)step-offset)*epsilon;
+      #ifdef INT_LEAPFROG
+        update_h(dtau);
+      #elif defined INT_OMELYAN
+        update_h(dtau);
+      #elif defined INT_2G1F
+        update_h_fermion(dtau);
+      #elif defined INT_3G1F
+        update_h_fermion(dtau);
+      #elif defined INT_5G1F
+        update_h_fermion(dtau);
+      #endif
     }
+
+    // Define values for first & last gauge update step (minor optimization)
+    #ifdef INT_LEAPFROG
+      node0_printf("using leapfrog integrator\n");
+      st1 = 0.5*epsilon; st2 = 0.5*epsilon;
+    #elif defined INT_OMELYAN
+      node0_printf("using 2nd-order Omelyan integrator\n");
+      st1 = lmbda*epsilon; st2 = lmbda*epsilon;
+    #elif defined INT_3G1F
+      node0_printf("using standard 3G1F integrator (MILC)\n");
+      st1 = (1.0/6.0-alpha/3.0)*epsilon;
+      st2 = (2.0-(11.0/6.0+alpha/3.0))*epsilon;
+    #else
+      node0_printf("No integration algorithm, or unknown one\n");
+      terminate(1);
+    #endif
 
     // First link smearing
     restore_fermion_links_from_site(fn_links, MILC_PRECISION);
 	fn = get_fm_links(fn_links);
 
     // Momentum/fermion heatbath
-    ranmom();
-    grsource_imp(F_OFFSET(phi), mass, EVEN, fn[0]); 
+    ranmom(); grsource_imp(F_OFFSET(phi), mass, EVEN, fn[0]); 
     
     // Calculate initial Hamiltonian
 	iters += ks_congrad( 
@@ -63,15 +92,50 @@ int update(){
     // Save backup gauge field
 	gauge_field_copy(F_OFFSET(link[0]), F_OFFSET(old_link[0]));
 
-    // MILC's convention for a 2nd-order Omelyan integrator
-    for(step=2; step <= steps; step+=2){
-        if (step == 2){update_u(0.5*lmbda*epsilon);}
-        else {update_u(lmbda*epsilon);}
-        update_v(step,0.333);
-        update_u((2.0-lmbda)*epsilon);
-        update_v(step,0.667);
-        if (step == steps){update_u(0.5*lmbda*epsilon);}
-    }
+    // Molecular dynamics trajectory
+    #ifdef INT_LEAPFROG
+      for(step=1; step <= steps; step++){
+        if (step == 1){update_u(st1);}
+        else {update_u(st1+st2);}
+        update_v(step,epsilon,0.5);
+        if (step == steps){update_u(st2);}
+      }
+    #elif defined INT_OMELYAN
+      for(step=1; step <= steps; step++){
+        if (step == 1){update_u(st1);}
+        else {update_u(st1+st2);}
+        update_v(step,0.5*epsilon,0.333);
+        update_u((1.0-2.0*lmbda)*epsilon);
+        update_v(step,0.5*epsilon,0.667);
+        if (step == steps){update_u(st2);}
+      }
+    #elif defined INT_3G1F // <--- MILC's default integrator for production
+      for(step=2; step <= steps; step+=2){
+        // First level
+        if (step == 2){update_u(st1);}
+        else {update_u(st1+st2);}
+	    update_h_gauge(epsilon/3.0);
+     	update_u( epsilon*( (0.5-beta)-(1.0/6.0-alpha/3.0) ) );
+	    update_v(step, epsilon, 0.333);
+     	update_u( epsilon*( (3.0/6.0+alpha/3.0)-(0.5-beta) ) );
+        update_h_gauge(epsilon/3.0);
+	    
+        // Second level
+        update_u( epsilon*( (5.0/6.0-alpha/3.0)-(3.0/6.0+alpha/3.0) ) );
+	    update_h_gauge(epsilon/3.0);
+     	update_u( epsilon*( (7.0/6.0+alpha/3.0)-(5.0/6.0-alpha/3.0) ) );
+        update_h_gauge(epsilon/3.0);
+        update_u( epsilon*( (9.0/6.0-alpha/3.0)-(7.0/6.0+alpha/3.0) ) );
+
+        // Third level
+        update_h_gauge(epsilon/3.0);
+        update_u( epsilon*( (1.5+beta)-(9.0/6.0-alpha/3.0) ) );
+        update_v(step, epsilon, 0.667);
+        update_u( epsilon*( (11.0/6.0+alpha/3.0)-(1.5+beta) ) );
+        update_h_gauge(epsilon/3.0);
+        if (step == steps){update_u(st2);}
+      }
+    #endif
 
     // Smear one last time & calculate final Hamiltonian
     next_cg_time = steps*epsilon;
@@ -91,11 +155,11 @@ int update(){
     broadcast_float(&xrandom);
     if( exp( (double)(startaction-endaction) ) < xrandom ){
 	if(steps > 0)
-	    gauge_field_copy(F_OFFSET(old_link[0]), F_OFFSET(link[0]));
-        #ifdef FN
-	        invalidate_fermion_links(fn_links);
-        #endif
-	    node0_printf("REJECT: delta S = %e\n", (double)(endaction-startaction));
+	  gauge_field_copy(F_OFFSET(old_link[0]), F_OFFSET(link[0]));
+      #ifdef FN
+	    invalidate_fermion_links(fn_links);
+      #endif
+	  node0_printf("REJECT: delta S = %e\n", (double)(endaction-startaction));
     }
     else {node0_printf("ACCEPT: delta S = %e\n", (double)(endaction-startaction));}
 
